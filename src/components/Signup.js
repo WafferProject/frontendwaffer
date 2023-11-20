135ea8964a9a47ee90fc121abd75e12d89f12dd9
salmen gharsellaoui <09126793s@gmail.com>
2023-11-20 23:19:42 +0100

    Buisness/consumer signin / signup + token done

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -3,162 +3,230 @@ import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
 import CssBaseline from "@mui/material/CssBaseline";
 import TextField from "@mui/material/TextField";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import Checkbox from "@mui/material/Checkbox";
 import Link from "@mui/material/Link";
 import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
 import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
-import ColorToggleButton from "./ColorToggleButton";
-import ToggleButton from "@mui/material/ToggleButton";
 import ToggleButtonGroup from "@mui/material/ToggleButtonGroup";
+import { ToggleButton } from "@mui/material";
 import "./Signup.css";
-function Copyright(props) {
-  return (
-    <Typography
-      variant="body2"
-      color="text.secondary"
-      align="center"
-      {...props}
-    >
-      {"Copyright © "}
-      <Link color="inherit" href="https://mui.com/">
-        Your Website
-      </Link>
-      {new Date().getFullYear()}
-      </Typography>
-  );
-}
-
-// TODO remove, this demo shouldn't need to reset the theme.
-
-const defaultTheme = createTheme();
+import axios from "axios";
 
 export default function SignUp() {
+  const [isConsumer, setConsumer] = React.useState(true);
+
+  // to do fix the routing
   const handleSubmit = (event) => {
     event.preventDefault();
-    const data = new FormData(event.currentTarget);
-    console.log({
-      email: data.get("email"),
-      password: data.get("password"),
-    });
+    const entity = isConsumer ? "consumer" : "buisness";
+    const url = `http://localhost:8080/api/${entity}/signup`;
+
+    const commonFormData = {
+      email: event.target.email.value,
+      password: event.target.password.value,
+      confirmPassword: event.target.confirm_password.value,
+      phone_number: event.target.phone.value,
+    };
+    const extraFormData = isConsumer
+      ? {
+          ...commonFormData,
+          first_name: event.target.first_name.value,
+          last_name: event.target.last_name.value,
+          occupation: event.target.occupation.value,
+        }
+      : {
+          ...commonFormData,
+          tax_registration_number: event.target.tax_registration_number.value,
+          opening_time: event.target.opening_time.value,
+          closing_time: event.target.closing_time.value,
+          name: event.target.business_name.value,
+          description: event.target.description.value,
+          location: [1, 3],
+        }; //to change location
+
+    console.log(commonFormData);
+    axios
+      .post(url, extraFormData)
+      .then(response => {
+        console.log(
+          "server responded from " +
+            url +
+            " with " +
+            JSON.stringify(response.data)
+        );
+      })
+      .catch(error => {
+        console.log("error  " + JSON.stringify(error.response.data.error));
+      });
   };
 
   return (
-    <div>
-      <ThemeProvider theme={defaultTheme}>
-        <Container component="main" maxWidth="xs">
-          <CssBaseline />
-          <Box
-            sx={{
-              marginTop: 8,
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <Box
+        sx={{
+          marginTop: 8,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign up
+        </Typography>
+        <ToggleButtonGroup
+          color="primary"
+          value={isConsumer}
+          exclusive
+          aria-label="Platform"
+        >
+          <ToggleButton
+            value={false}
+            onClick={() => {
+              setConsumer(false);
             }}
           >
-            <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
-              <LockOutlinedIcon />
-            </Avatar>
-            <Typography component="h1" variant="h5">
-              Sign up
-            </Typography>
-            <ColorToggleButton />
-            <Box
-              component="form"
-              noValidate
-              onSubmit={handleSubmit}
-              sx={{ mt: 3 }}
-            >
-              <Grid container spacing={2}>
+            Business
+          </ToggleButton>
+          <ToggleButton
+            value={true}
+            onClick={() => {
+              setConsumer(true);
+            }}
+          >
+            Consumer
+          </ToggleButton>
+        </ToggleButtonGroup>
+
+        <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+          <Grid container spacing={2}>
+            <Grid className="field" item xs={12}>
+              <TextField
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+              />
+            </Grid>
+            <Grid className="field" item xs={12}>
+              <TextField
+                required
+                fullWidth
+                id="password"
+                label="Password"
+                name="password"
+                type="password"
+                autoComplete="new-password"
+              />
+            </Grid>
+            <Grid className="field" item xs={12}>
+              <TextField
+                required
+                fullWidth
+                id="confirm_password"
+                label="Confirm Password"
+                name="confirmPassword"
+                type="password"
+                autoComplete="new-password"
+              />
+            </Grid>
+            <Grid className="field" item xs={12}>
+              <TextField
+                required
+                fullWidth
+                id="phone"
+                label="Phone"
+                name="phone"
+                autoComplete="phone"
+              />
+            </Grid>
+
+            {isConsumer ? (
+              <>
                 <Grid className="field" item xs={12}>
                   <TextField
                     required
                     fullWidth
-                    id="businessName"
+                    id="first_name"
+                    label="first name"
+                    name="first_name"
+                    autoComplete="first_name"
+                  />
+                </Grid>
+                <Grid className="field" item xs={12}>
+                  <TextField
+                    required
+                    fullWidth
+                    id="last_name"
+                    label="last name"
+                    name="last_name"
+                    autoComplete="last_name"
+                  />
+                </Grid>
+                <Grid className="field" item xs={12}>
+                  <TextField
+                    required
+                    fullWidth
+                    id="occupation"
+                    label="occupation"
+                    name="occupation"
+                    autoComplete="occupation"
+                  />
+                </Grid>{" "}
+              </>
+            ) : (
+              <>
+                <Grid className="field" item xs={12}>
+                  <TextField
+                    required
+                    fullWidth
+                    id="business_name"
                     label="Business Name"
                     name="businessName"
                     autoComplete="business-name"
                   />
                 </Grid>
-                <Grid className="field" item xs={12}>
+
+                <Grid item xs={12}>
                   <TextField
                     required
                     fullWidth
-                    id="email"
-                    label="Email Address"
-                    name="email"
-                    autoComplete="email"
-                  />
-                </Grid>
-                <Grid className="field" item xs={12}>
-                  <TextField
-                    required
-                    fullWidth
-                    id="taxRegistrationNumber"
+                    id="tax_registration_number"
                     label="Tax Registration Number"
                     name="taxRegistrationNumber"
                     autoComplete="tax-registration-number"
                   />
                 </Grid>
-                <Grid className="field" item xs={12}>
+                <Grid item xs={12} sm={6}>
                   <TextField
                     required
                     fullWidth
-                    id="password"
-                    label="Password"
-                    name="password"
-                    type="password"
-                    autoComplete="new-password"
-                  />
-                </Grid>
-                <Grid className="field" item xs={12}>
-                  <TextField
-                    required
-                    fullWidth
-                    id="confirmPassword"
-                    label="Confirm Password"
-                    name="confirmPassword"
-                    type="password"
-                    autoComplete="new-password"
-                  />
-                </Grid>
-                <Grid className="field" item xs={12}>
-                  <TextField
-                    required
-                    fullWidth
-                    id="phone"
-                    label="Phone"
-                    name="phone"
-                    autoComplete="phone"
-                  />
-                </Grid>
-                <Grid className="field-time" item xs={12} sm={6}>
-                  <TextField
-                    required
-                    fullWidth
-                    id="openingTime"
+                    id="opening_time"
                     label="Opening Time"
                     name="openingTime"
                     type="time"
                     autoComplete="opening-time"
                   />
                 </Grid>
-                <Grid className="field-time" item xs={12} sm={6}>
+                <Grid item xs={12} sm={6}>
                   <TextField
                     required
                     fullWidth
-                    id="closingTime"
+                    id="closing_time"
                     label="Closing Time"
                     name="closingTime"
                     type="time"
                     autoComplete="closing-time"
                   />
                 </Grid>
-                <Grid className="field" item xs={12}>
+                <Grid item xs={"auto"}>
                   <TextField
                     required
                     fullWidth
@@ -168,7 +236,7 @@ export default function SignUp() {
                     autoComplete="location"
                   />
                 </Grid>
-                <Grid className="field" item xs={12}>
+                <Grid item xs={12}>
                   <TextField
                     required
                     fullWidth
@@ -178,36 +246,27 @@ export default function SignUp() {
                     autoComplete="description"
                   />
                 </Grid>
+              </>
+            )}
 
-                <Grid item xs={12}>
-                  <FormControlLabel
-                    control={
-                      <Checkbox value="allowExtraEmails" color="primary" />
-                    }
-                    label="I want to receive inspiration, marketing promotions and updates via email."
-                  />
-                </Grid>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign Up
+            </Button>
+            <Grid container justifyContent="flex-end">
+              <Grid item>
+                <Link href="#" variant="body2">
+                  Already have an account? Sign in
+                </Link>
               </Grid>
-              <Button
-                type="submit"
-                fullWidth
-                variant="contained"
-                sx={{ mt: 3, mb: 2 }}
-              >
-                Sign Up
-              </Button>
-              <Grid container justifyContent="flex-end">
-                <Grid item>
-                  <Link href="#" variant="body2">
-                    Already have an account? Sign in
-                  </Link>
-                </Grid>
-              </Grid>
-            </Box>
-          </Box>
-          <Copyright sx={{ mt: 5 }} />
-        </Container>
-      </ThemeProvider>
-    </div>
+            </Grid>
+          </Grid>
+        </Box>
+      </Box>
+    </Container>
   );
 }
